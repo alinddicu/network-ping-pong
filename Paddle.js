e7c93f8d3759985c641f3b375cb374e5acdb1dd4
alinddicu <alinddicu@yahoo.com>
2014-09-27 17:05:42 +0200

    Rewritting objects so they can be node modules

--- Paddle.js
+++ Paddle.js
@@ -1,20 +1,24 @@
 
 function Paddle(pos, tableWidth, tableHeight) {
-	var self = this;
-	
-	self.h = 5;
-	self.w = 150;
-	self.name = pos;
-	
-	self.x = tableWidth/2 - self.w/2;
-	self.y = (self.name == "top") ? 0 : tableHeight - self.h;	
-	
-	self.draw = function(drawCtx){	
-		drawCtx.fillStyle = "white";
-		drawCtx.fillRect(self.x, self.y, self.w, self.h);
-	};
-	
-	self.move = function(mousePointerX){
-		self.x =  mousePointerX - self.w/2;
-	};
+
+	this.h = 5;
+	this.w = 150;
+	this.name = pos;
+	this.x = tableWidth/2 - this.w/2;
+	this.y = (this.name == "top") ? 0 : tableHeight - this.h;
 };
+
+Paddle.prototype.log = function(){
+		console.log('Paddle x:' + this.x);
+};
+
+Paddle.prototype.draw = function(drawCtx){
+	drawCtx.fillStyle = "white";
+	drawCtx.fillRect(this.x, this.y, this.w, this.h);
+};
+
+Paddle.prototype.move = function(mousePointerX){
+	this.x =  mousePointerX - this.w/2;
+};
+
+module.exports = Paddle;
